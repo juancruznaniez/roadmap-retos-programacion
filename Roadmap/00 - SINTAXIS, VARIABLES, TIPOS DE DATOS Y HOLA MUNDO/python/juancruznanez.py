3b77d2b8ec3368eb6941fe0c3f8d35da5851f153
juancruznaniez <juancruznaniez@hotmail.com>
2024-06-10 14:39:31 -0300

    Create juancruznanez.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/juancruznanez.py
@@ -0,0 +1,57 @@
+# EJERCICIO:
+#  - Crea un comentario en el código y coloca la URL del sitio web oficial del
+#    lenguaje de programación que has seleccionado.
+#  - Representa las diferentes sintaxis que existen de crear comentarios
+#    en el lenguaje (en una línea, varias...).
+#  - Crea una variable (y una constante si el lenguaje lo soporta).
+#  - Crea variables representando todos los tipos de datos primitivos
+#    del lenguaje (cadenas de texto, enteros, booleanos...).
+#  - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+# 
+#  ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+#  debemos comenzar por el principio.
+# /
+
+'''
+www.python.org
+'''
+
+# www.python.org
+
+# Forma de declarar una variable en Python
+variable = "Juan"
+
+# En Python no existen las constantes como tal, por ende por convención se escriben en mayúsculas.
+VARIABLE_COSTANTE = "Juan"
+
+# DATOS PRIMITIVOS:
+
+# Enteros (int)
+entero = 1
+
+# Decimales (float)
+decimal = 1.50
+
+# Números complejos (complex)
+complejo = 2j + 1
+
+# Cadenas de texto (str)
+cadena = "Python"
+
+# Booleano (bool)
+booleano = True
+
+# Valor nulo (none)
+nulo = None
+
+
+# Imprimiendo por terminal: 
+
+print("¡Hola, Python!")
+# Imprimiendo con f-strings
+print(f"¡Hola, {variable}!")    
+# Imprimiendo con el metodo format()
+print("¡Hola, {}!".format(variable))
+
+
+
